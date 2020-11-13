bbc3f4739d114bf3c5702ab91853906a9a74ae25
Jukka Laitinen <jukkax@ssrc.tii.ae>
2020-11-13 15:10:15 +0200

    Add path to px4_msgs to the packaging script for px4_ros_com
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- packaging/package.sh
+++ packaging/package.sh
@@ -16,11 +16,13 @@ pushd ../ros2_ws/src/px4_mavlink_ctrl
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && sed -i 's/^\tdh_shlibdeps.*/& --dpkg-shlibdeps-params=--ignore-missing-info/g' debian/rules && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
 popd
 
-pushd ../ros2_ws/src/px4_ros_com
+pushd ../ros2_ws/src/px4_msgs
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
+# The following px4_ros_com needs px4_msgs, which was just compiled, so add it to the CMAKE paths
+export CMAKE_PREFIX_PATH=${PWD}/debian/ros-foxy-px4-msgs/opt/ros/foxy
 popd
 
-pushd ../ros2_ws/src/px4_msgs
+pushd ../ros2_ws/src/px4_ros_com
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
 popd
 
