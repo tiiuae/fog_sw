7174c8aa1a2047f58eee3aaca4b89690a86c88bb
Jari Nippula <jarix@ssrc.tii.ae>
2021-01-15 12:14:39 +0200

    Update communication link with gstnode

--- packaging/communication_link/package.sh
+++ packaging/communication_link/package.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 get_version() {
-    pushd ../../ros2_ws/src/communication_link
-    version=1.0.0~$(git describe --always --tags --dirty --match "[0-9]*.[0-9]*.[0-9]*")
-    echo ${version}
-    popd
+	pushd ../../ros2_ws/src/communication_link
+	version=1.0.0~$(git describe --always --tags --dirty --match "[0-9]*.[0-9]*.[0-9]*")
+	echo ${version}
+	popd
 }
 
 build() {
+	export CGO_CFLAGS="-I$(realpath ../../ros2_ws/src/px4_msgs/debian/ros-foxy-px4-msgs/opt/ros/foxy/include)"
+	export CGO_LDFLAGS="-L$(realpath ../../ros2_ws/src/px4_msgs/debian/ros-foxy-px4-msgs/opt/ros/foxy/lib)"
+	echo "flags: $CGO_CFLAGS"
 	pushd ../../ros2_ws/src/communication_link
-	export GOPRIVATE=github.com/ssrc-tii
-	source /opt/ros/foxy/setup.bash
 	go build
-	cp communication_link ../../../packaging/communication_link/ && go clean
-        popd
+	cp -f communication_link ../../../packaging/communication_link/ && go clean
+	popd
 }
 
 make_deb() {
@@ -30,7 +31,7 @@ make_deb() {
 	get_version
 	sed -i "s/VERSION/${version}/" ${build_dir}/DEBIAN/control
 	cat ${build_dir}/DEBIAN/control
-        echo communication-link_${version}_amd64.deb
+	echo communication-link_${version}_amd64.deb
 	fakeroot dpkg-deb --build ${build_dir} ../communication-link_${version}_amd64.deb
 	rm -rf ${build_dir}
 	echo "Done"
