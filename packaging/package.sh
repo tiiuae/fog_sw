e27deaf9b7ee9ecf45df3774a3666aae2ccdf127
Jari Nippula <jarix@ssrc.tii.ae>
2021-02-11 13:52:42 +0200

    Debian packaging for indoor_pos node

--- packaging/package.sh
+++ packaging/package.sh
@@ -26,6 +26,10 @@ pushd ../ros2_ws/src/px4_ros_com
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
 popd
 
+pushd ../ros2_ws/src/indoor_pos
+bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
+popd
+
 pushd ../simulation/src/sim_mesh_ctrl
 bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary && mv ../*.deb ../../../packaging/
 popd
