6d1f8a50294f658c0fb91beeb032838c9284c2a5
Kalle Koivisto <kalle.koivisto@unikie.com>
2021-04-30 13:35:42 +0300

    Added SROS identity definitions and commented out the log path

--- packaging/systemd/setup_fog.sh
+++ packaging/systemd/setup_fog.sh
@@ -10,11 +10,21 @@ export DRONE_DEVICE_ID
 DIR="/enclave/sros"
 if [ -d "$DIR" ]; then
   echo "Folder ${DIR} exists. Enabling SROS2."
+
+  # Source sros identity for this environment
+  . /enclave/sros_identity
+
+  # Custom variables
+  export SROS_IDENTITY
+  export SROS_ENCLAVE="/${DRONE_DEVICE_ID}/${SROS_IDENTITY}"
+  export SROS_LAUNCH_ARGS="--ros-args --enclave ${SROS_ENCLAVE}"
+
+  # Actual ROS SECURITY variables
   export ROS_SECURITY_ENABLE=true
   export ROS_SECURITY_STRATEGY=Enforce
   export ROS_SECURITY_KEYSTORE="${DIR}"
   export ROS_SECURITY_LOG_VERBOSITY=WARN
-  export ROS_SECURITY_LOG_FILE="${DIR}/log/ros-security.log"
+  #export ROS_SECURITY_LOG_FILE="${DIR}/log/ros-security.log"
 else
   echo "Warning: ${DIR} not found. Will not enable SROS2."
 fi
