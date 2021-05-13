191e6c46e0a5d92544d451569a0adf7707b5e754
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2021-05-13 15:21:23 +0200

    All F4F commits SQUASHED combination of 31 commits:
    * added tmux and tmuxinator submodules
    
    * added installation script for tmux and tmuxinator
    
    * added ros-related dependencies
    
    * added octomap submodule
    
    * added fog_msgs and control_interface
    
    * added navigation submodule
    
    * added simulation tmuxinator session
    
    * added gazebo_resources submodule
    
    * updated tmux session with basic px4 build
    
    * update simulation tmux session
    
    * update submodules
    
    * updated gazebo_resources submodule
    
    * updated fog_core pkg
    
    * update tmux session for simulation
    
    * update octomap_server2 submodule
    
    * update navigation input topic
    
    * update tmux conf to be able to kill all children
    
    * updated gazebo_resouces submodule
    
    * update rosdep to be able compile deb packages
    
    * update navigation input to accept nav_msgs::Path
    
    * updared navigation and fog_msgs repos
    
    * added script for creating archives from f4f packages
    
    * update submodules url
    
    * uav_name -> drone_device_id
    
    * update control_interface submodule
    
    * fixed killing of tmux session
    
    * update submodules
    
    * update submodules, change to px4_ros_com to F4F fork
    
    * update submodules
    
    * update control core to 0.0.2
    
    * update fog_gazebo_resources and navigation
    
     Author:    Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
     Date:      Thu May 13 15:21:23 2021 +0200

---
+++ installation/dependencies/tmuxinator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+cd $MY_PATH/../../tools/tmuxinator
+
+echo "$0: installing tmuxinator"
+
+sudo apt-get -y install ruby gem
+
+gem build tmuxinator.gemspec
+sudo gem install tmuxinator -v 1.1.5
