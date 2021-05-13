d69934a067a60283f77bab614309be442412b8e9
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2021-05-13 15:21:23 +0200

    All F4F commits SQUASHED combination of 31 commits:
    * added tmux and tmuxinator submodules
    
    * added installation script for tmux and tmuxinator
    
    * added ros-related dependencies
    
    * added octomap submodule
    
    * added fog_msgs and control_interface
    
    * added navigation submodule
    
    * added simulation tmuxinator session
    
    * added gazebo_resources submodule
    
    * updated tmux session with basic px4 build
    
    * update simulation tmux session
    
    * update submodules
    
    * updated gazebo_resources submodule
    
    * updated fog_core pkg
    
    * update tmux session for simulation
    
    * update octomap_server2 submodule
    
    * update navigation input topic
    
    * update tmux conf to be able to kill all children
    
    * updated gazebo_resouces submodule
    
    * update rosdep to be able compile deb packages
    
    * update navigation input to accept nav_msgs::Path
    
    * updared navigation and fog_msgs repos
    
    * added script for creating archives from f4f packages
    
    * update submodules url
    
    * uav_name -> drone_device_id
    
    * update control_interface submodule
    
    * fixed killing of tmux session
    
    * update submodules
    
    * update submodules, change to px4_ros_com to F4F fork
    
    * update submodules
    
    * update control core to 0.0.2
    
    * update fog_gazebo_resources and navigation
    
     Author:    Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
     Date:      Thu May 13 15:21:23 2021 +0200

---
+++ packaging/f4f_package.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+options=$(getopt -l "create,clean" -o "" -a -- "$@")
+
+eval set -- "$options"
+
+while true
+do
+  case $1 in
+    --create)
+      CREATE=true
+      shift
+      ;;
+    --clean)
+      CLEAN=true
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+  esac
+done
+
+[ -z "$CREATE" ] && [ -z "$CLEAN" ] && echo "$0: Choose --create, --clean, or both" && exit 1
+
+cd "$MY_PATH"
+
+## | ---------  cleaning after creation of debian packages  --------- |
+
+function clean_package() {
+
+  if [ -n "$1" ]; then
+    pushd ../ros2_ws/src/$1 > /dev/null
+  fi
+  echo "Cleaned \"`basename "$PWD"`\""
+  if [ -d debian ]; then
+    rm -rf debian  
+  fi
+  if [ -d obj-x86_64-linux-gnu ]; then
+    rm -rf debian obj-x86_64-linux-gnu 
+  fi
+  if [ -n "$1" ]; then
+    popd > /dev/null
+  fi
+}
+
+if [ -n "$CLEAN" ];
+then
+
+  echo "$0: Removing helper archive folders"
+  clean_package fog_msgs
+  clean_package fog_core
+  clean_package fog_gazebo_resources
+  clean_package rplidar_ros2
+  clean_package octomap_server2
+  clean_package control_interface
+  clean_package navigation
+  clean_package control_interface
+
+fi
+
+## | ---------  create debian packages for these pkg  --------- |
+
+if [ -n "$CREATE" ];
+then
+
+  function move_archives() {
+    if [ -f ../*.deb ]; then
+      mv ../*.deb ../../../packaging/deb_files/
+    else
+      echo -e "\e[01;31mDeb file doesn't exist. Clean the helper archive files -> Call \"f4f_package.sh --clean\"\e[0m"
+      exit 1
+    fi
+    if [ -f ../*.ddeb ]; then
+      mv ../*.ddeb ../../../packaging/deb_files/
+    fi
+  }
+
+  pushd ../ros2_ws/src/fog_msgs > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary 
+  move_archives
+  export CMAKE_PREFIX_PATH=${PWD}/debian/ros-foxy-fog-msgs/opt/ros/foxy
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/fog_core > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary 
+  move_archives
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/fog_gazebo_resources > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary 
+  move_archives
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/rplidar_ros2 > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary 
+  move_archives
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/octomap_server2 > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary
+  move_archives
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/navigation > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary 
+  move_archives
+  popd > /dev/null
+
+  pushd ../ros2_ws/src/octomap_server2 > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary
+  move_archives
+  popd > /dev/null
+
+  # package that depends on mavsdk needs little hack
+  pushd ../ros2_ws/src/control_interface > /dev/null
+  bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && sed -i 's/^\tdh_shlibdeps.*/& --dpkg-shlibdeps-params=--ignore-missing-info/g' debian/rules && fakeroot debian/rules binary 
+  move_archives
+  popd > /dev/null
+
+fi
