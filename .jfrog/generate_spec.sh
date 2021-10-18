2d310f389b0f21c69053fc3a553019558153c46d
Esa Kulmala <esa.kulmala@unikie.com>
2021-10-18 14:52:01 +0300

    rename to genate_spec.sh

--- .jfrog/generate_spec.sh
+++ .jfrog/generate_spec.sh
@@ -6,7 +6,6 @@ OUTDIR=$1
 THISDIR="$(dirname "$(readlink -f "$0")")"
 
 template="${THISDIR}"/spec.template
-packages="${OUTDIR}"/packages
 spec="${OUTDIR}"/spec.json
 
 pushd "${THISDIR}/.."
@@ -15,19 +14,18 @@ while read -r line
 do
   sha=$(echo "$line" | awk '{print $1}' )
   submodule=$(echo "$line" | awk '{print $2}' )
+  echo "found $submodule => $sha"
   modules[$submodule]=$sha
 done <<< "$(git submodule status)"
 popd
 
-mkdir -p "${packages}"
+mkdir -p "${OUTDIR}"
 cp "${template}" "${spec}"
+
+echo "generate $spec..."
 
 for submodule in "${!modules[@]}"
 do
   sha="${modules[$submodule]}"
   sed -i "s|\${$submodule}|${sha}|" "${spec}"
 done
-
-pushd "${OUTDIR}"
-jfrog rt dl --spec="${spec}"
-popd
