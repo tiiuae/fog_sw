a44b834a647c2976677c61e93c2e3a0bc051186a
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2021-12-15 19:37:06 +0200

    Downgrade navigation nodes and update camera node

--- tasks.py
+++ tasks.py
@@ -91,6 +91,9 @@ def build_system(c):
     Build drivers and other system components
     """
     c.run("docker run --rm -v %s:/fog_sw fogsw-builder /fog_sw/packaging/package_sys.sh" % THISDIR)
+    # fogsw-tools
+    with c.cd("%s/fogsw_tools" % THISDIR):
+        c.run("./build.sh %s/packaging/deb_files" % THISDIR)
 
 @task(buildenv)
 def build_kernel(c):
